d7a67ce018e84aa3214a8cc41f265f631fde2768
Miroslav Belov <miroslavbelov8@gmail.com>
2022-06-03 14:47:35 +0300

    [AZE-2] add LetterI

--- src/letters.ts
+++ src/letters.ts
@@ -32,3 +32,22 @@ export enum LetterE {
   Y,
   Z,
 }
+
+export interface LetterI {
+  /**
+   * Returns the underlying letter as a member of {@link LetterE}.
+   */
+  letter(): LetterE;
+  /**
+   * Returns this letter in Latin script (1992-nowadays).
+   *
+   * @see {@link https://en.wikipedia.org/wiki/Azerbaijani_alphabet#Transliteration|Azerbaijani alphabet}
+   */
+  asLatin(): string;
+  /**
+   * Returns this letter in Cyrillic script (1958–1991).
+   *
+   * @see {@link https://en.wikipedia.org/wiki/Azerbaijani_alphabet#Transliteration|Azerbaijani alphabet}
+   */
+  asCyrillic(): string;
+}
